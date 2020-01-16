c5dde04a7ec4b248e7c4502afa2e7a2c42117918
yuta-san765 <nitoplus765@gmail.com>
2020-01-16 17:46:46 +0900

    Fixed error, show timeline

--- js/timeline.js
+++ js/timeline.js
@@ -1,5 +1,6 @@
 'use strict';
 {
+  // ロード時にタイムライン表示
   window.onload = function(){
     const postsUrl = 'https://teachapi.herokuapp.com/posts';
     const postsParamas = {
@@ -19,15 +20,44 @@
       return response.json();
     })
     .then(json => {
-      console.log(json);
-      document.querySelector('posted-items').removeAttribute('hidden');
-      for(let i = 0, len = json.length; i < len; i++){
-        document.querySelector('for-name').textContent = json[i].user.name;
-        document.querySelector('for-bio').textContent = json[i].user.bio;
-        document.querySelector('for-text').textContent = json[i].text;
-        const posts_prime = document.querySelector('posted-items').cloneNode(true);
-        document.main.appendChild(posts_prime);
-      }
+      // console.log(json);
+      let timeLine = '';
+      json.forEach(element => {
+        // console.log(element);
+        timeLine +=
+          `
+          <div class="posted-item">
+            <div class="for-img"></div>
+            <div class="for-post">
+              <div class="for-name-bio">
+                <span class="for-name">${element.user.name}</span>
+                <span class="for-bio">${element.user.bio}</span>
+              </div>
+              <div class="for-text">${element.text}</div>
+            </div>
+          </div>`;
+
+      });
+      // console.log(timeLine);
+      document.querySelector('.timeline').innerHTML = timeLine;
+      // for(let i = 0, len = json.length; i < len; i++){ //失敗の跡地
+      //   document.querySelector('.timeline').innerHTML = `
+      //   <div class="posted-item">
+      //     <div class="for-img"></div>
+      //     <div class="for-post">
+      //       <div class="for-name-bio">
+      //         <span class="for-name"></span>
+      //         <span class="for-bio"></span>
+      //       </div>
+      //       <div class="for-text"></div>
+      //     </div>
+      //   </div>`;
+      // //   document.querySelector('.for-name').textContent = json[i].user.name;
+      //   document.querySelector('.for-bio').textContent = json[i].user.bio;
+      //   document.querySelector('.for-text').textContent = json[i].text;
+      //   const posts_prime = document.querySelector('.posted-item').cloneNode(true);
+      //   document.querySelector('.timeline').appendChild(posts_prime);
+      // }
     })
     .catch(error => {
       console.error(error);
